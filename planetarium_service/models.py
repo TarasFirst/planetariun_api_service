229f4acab10129876351f35e65826ab8f55628ed
TarasFirst <firsttaras@mail.com>
2024-08-27 14:51:52 +0200

    create model AstronomyShow

--- planetarium_service/models.py
+++ planetarium_service/models.py
@@ -6,3 +6,11 @@ class ShowTheme(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class AstronomyShow(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
