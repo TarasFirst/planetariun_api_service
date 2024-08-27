0de6142a3811b5147c8922849e06f87d69420f8b
TarasFirst <firsttaras@mail.com>
2024-08-27 14:54:10 +0200

    add property "capacity" to model PlanetariumDome

--- planetarium_service/models.py
+++ planetarium_service/models.py
@@ -20,3 +20,10 @@ class PlanetariumDome(models.Model):
     name = models.CharField(max_length=255)
     rows = models.IntegerField()
     seats_in_row = models.IntegerField()
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+    def __str__(self):
+        return self.name
