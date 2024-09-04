e8d0bffaa0fc233b0afdd703241c81cffa48b52f
TarasFirst <firsttaras@mail.com>
2024-09-04 15:26:36 +0200

    fix length of string

--- planetarium_api_service/urls.py
+++ planetarium_api_service/urls.py
@@ -27,7 +27,11 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/planetarium_service/", include("planetarium_service.urls", namespace="planetarium_service")),
+    path(
+        "api/planetarium_service/",
+        include("planetarium_service.urls",
+                namespace="planetarium_service")
+    ),
     path("api/user/", include("user.urls", namespace="user")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
